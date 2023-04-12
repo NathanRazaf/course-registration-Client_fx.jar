f2f97dba6cac83458d1855e7f497688de34bb838
Cynthia Phan <cynthiaphan.code@gmail.com>
2023-04-12 17:21:08 -0400

    edit sendRegistration : returns a String

--- src/main/java/client_fx/Model.java
+++ src/main/java/client_fx/Model.java
@@ -22,13 +22,16 @@ public class Model {
         return (ArrayList<Course>) ois.readObject();
     }
 
-    public void sendRegistration(String prenom, String nom, String email, String matricule, Course course) throws IOException {
+    public String sendRegistration(String prenom, String nom, String email, String matricule, Course course) throws IOException, ClassNotFoundException {
         Socket socket = new Socket("127.0.0.1", 1337);
         ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
         oos.writeObject(REGISTER_COMMAND);
         oos.writeObject(new RegistrationForm(prenom, nom, email, matricule, course));
+
+        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+        String message = (String) ois.readObject();
         socket.close();
+
+        return message;
     }
-
-
 }
