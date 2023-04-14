fdc910bc1c552a63daa28cd0235218c0b5e31cdd
Cynthia Phan <cynthiaphan.code@gmail.com>
2023-04-14 10:56:13 -0400

    add JavaFX library to project and create RunMain class to generate client_fx.jar

---
+++ javadoc/client/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"com.example.demo1","l":"client_fx"},{"m":"com.example.demo1","l":"server"},{"m":"com.example.demo1","l":"server.models"}];updateSearchResults();
---
+++ src/main/java/client_fx/RunMain.java
@@ -0,0 +1,16 @@
+package client_fx;
+
+/**
+ * La classe RunMain a été créée pour pouvoir lancer l'application à partir de la ligne de commande.
+ */
+public class RunMain {
+    /**
+     * La méthode main exécute la méthode main de la classe Main.
+     * main prend comme paramètre args, une liste de String, qui représente un argument de la ligne de commande.
+     *
+     * @param args arguments de la ligne de commande
+     */
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
